Naming correct but need to resolve some numerical instability

node_attribution/bloom_for_node_attribution.py
@@ -21,8 +21,9 @@ class BloomMLPForNodeAttribution(BloomMLP):
     def forward(self, hidden_states: torch.Tensor, residual: torch.Tensor) -> Tuple[torch.Tensor, Dict[str, torch.tensor]]:
         activations = {} # For node attribution
         
-        hidden_states = self.gelu_impl(self.dense_h_to_4h(hidden_states))
         activations["dense_h_to_4h"] = hidden_states # For node attribution
+        hidden_states = self.gelu_impl(self.dense_h_to_4h(hidden_states))
+        
 
         if self.pretraining_tp > 1 and self.slow_but_exact:
             intermediate_output = torch.zeros_like(residual)
@@ -33,11 +34,10 @@ class BloomMLPForNodeAttribution(BloomMLP):
                     self.dense_4h_to_h.weight[:, int(i * slices) : int((i + 1) * slices)],
                 )
         else:
+            activations["dense_4h_to_h"] = hidden_states # For node attribution
             intermediate_output = self.dense_4h_to_h(hidden_states)
 
-        activations["dense_4h_to_h"] = intermediate_output # For node attribution
         output = dropout_add(intermediate_output, residual, self.hidden_dropout, self.training)
-        activations["dense_4h_to_h_post_dropout"] = output # For node attribution
         
 
         return output, activations # For node attribution
@@ -58,8 +58,7 @@ class BloomAttentionForNodeAttribution(BloomAttention):
         activations = {} # For node attribution
         activations["query_key_value"] = hidden_states # For node attribution
         fused_qkv = self.query_key_value(hidden_states)  # [batch_size, seq_length, 3 x hidden_size]
-         
-
+        
         # 3 x [batch_size, seq_length, num_heads, head_dim]
         (query_layer, key_layer, value_layer) = self._split_heads(fused_qkv)
         
@@ -70,9 +69,6 @@ class BloomAttentionForNodeAttribution(BloomAttention):
         key_layer = key_layer.permute(0, 2, 3, 1).reshape(batch_size * self.num_heads, self.head_dim, q_length)
         value_layer = value_layer.transpose(1, 2).reshape(batch_size * self.num_heads, q_length, self.head_dim)
         if layer_past is not None:
-            activations["key_pre_past_concat"] = key_layer
-            activations["value_pre_past_concat"] = value_layer
-            
             past_key, past_value = layer_past
             # concatenate along seq_length dimension:
             #  - key: [batch_size * self.num_heads, head_dim, kv_length]
@@ -99,8 +95,6 @@ class BloomAttentionForNodeAttribution(BloomAttention):
             beta=self.beta,
             alpha=self.inv_norm_factor,
         )
-        
-        activations["query_key_matmul"] = matmul_result
 
         # change view to [batch_size, num_heads, q_length, kv_length]
         attention_scores = matmul_result.view(batch_size, self.num_heads, q_length, kv_length)
@@ -122,15 +116,13 @@ class BloomAttentionForNodeAttribution(BloomAttention):
 
         # change view [batch_size x num_heads, q_length, kv_length]
         attention_probs_reshaped = attention_probs.view(batch_size * self.num_heads, q_length, kv_length)
-        activations["attention_probs_reshaped"] = attention_probs_reshaped
 
         # matmul: [batch_size * num_heads, q_length, head_dim]
+        activations["attention_probs_reshaped"] = attention_probs_reshaped
         context_layer = torch.bmm(attention_probs_reshaped, value_layer)
-        activations["context_layer"] = context_layer
 
         # change view [batch_size, num_heads, q_length, head_dim]
         context_layer = self._merge_heads(context_layer)
-        activations["merge_heads"] = context_layer
 
         # aggregate results across tp ranks. See here: https://github.com/pytorch/pytorch/issues/76232
         if self.pretraining_tp > 1 and self.slow_but_exact:
@@ -142,8 +134,9 @@ class BloomAttentionForNodeAttribution(BloomAttention):
                     self.dense.weight[:, int(i * slices) : int((i + 1) * slices)],
                 )
         else:
+            activations["dense"] = context_layer # For node attribution
             output_tensor = self.dense(context_layer)
-            activations["dense"] = output_tensor # For node attribution
+            
 
         output_tensor = dropout_add(output_tensor, residual, self.hidden_dropout, self.training)
 
@@ -212,8 +205,7 @@ class BloomBlockForNodeAttribution(BloomBlock):
         outputs = attn_outputs[1:]
 
         layernorm_output = self.post_attention_layernorm(attention_output)
-        activations["post_attention_layernorm"] = layernorm_output # For node attribution
-
+    
         # Get residual
         if self.apply_residual_connection_post_layernorm:
             residual = layernorm_output
@@ -389,7 +381,6 @@ class BloomModelForNodeAttribution(BloomModel):
         
         # Add last hidden state
         hidden_states = self.ln_f(hidden_states)
-        transformer_activations["ln_f"] = hidden_states # For node attribution
 
         if output_hidden_states:
             all_hidden_states = all_hidden_states + (hidden_states,)
@@ -460,7 +451,8 @@ class BloomForCausalLMForNodeAttribution(BloomForCausalLM, GenerationMixinForNod
         )
         hidden_states = transformer_outputs[0]
         activations["transformer"] = transformer_outputs.activations # For node attribution
-
+        
+        activations["lm_head"] = hidden_states
         lm_logits = self.lm_head(hidden_states)
         
         loss = None

node_attribution/node_attribution.py
@@ -7,7 +7,7 @@ from bloom_for_node_attribution import BloomForCausalLMForNodeAttribution
 from utils import count_params
 
 class NodeAttributor:
-    def __init__(self, model_size, data):
+    def __init__(self, model_size):
         self.load_bloom_model(model_size)
 
     def load_bloom_model(self, model_size):
@@ -34,7 +34,7 @@ class NodeAttributor:
                     Total Param Count (w/ LM Head): {self.total_params:,}"
         )
 
-    def forward_pass(self, model, tokenizer, line):
+    def forward_pass(self, line):
         inputs = self.tokenizer(line, return_tensors="pt")
         outputs = self.model.generate(
             input_ids=inputs["input_ids"], 
@@ -54,7 +54,7 @@ class NodeAttributor:
         contributions = []
         
         for line in data:
-            outputs, _, seq_length = self.forward_pass(self.model, self.tokenizer, line)
+            outputs, _, seq_length = self.forward_pass(line)
             sequence = outputs.sequences[0]
             calculate = SequenceContributionCalculator(
                 model_params = self.model_params,
@@ -68,17 +68,21 @@ class NodeAttributor:
             calculate.lm_head_contributions()
             
             # Transformer blocks
-            for block_id in range(self.num_blocks - 1, -1):
+            for block_id in reversed(range(self.num_blocks)):
+                print(block_id)
                 block_id = str(block_id)
                 calculate.transformer_block_contributions(block_id)
                 
             contributions.append(calculate.contributions)
+        
+        return contributions
             
 class SequenceContributionCalculator:
-    def __init__(self, model_params, model_activations, sequence, num_heads, head_dim):
+    def __init__(self, model_params, model_activations, sequence, seq_length, num_heads, head_dim):
         self.model_params = model_params
         self.model_activations = model_activations
         self.sequence = sequence
+        self.seq_length = seq_length
         self.num_heads = num_heads
         self.head_dim = head_dim
         self.contributions = []
@@ -88,16 +92,19 @@ class SequenceContributionCalculator:
     def lm_head_contributions(self):
         """Final hidden states to token logit contributions"""
         
+        layer_name = "lm_head"
+        param_name = f"{layer_name}.weight"
+        hidden_state_activations = self.model_activations[layer_name].squeeze()
+        
         # weights have shape: (vocab_size x hidden size)
-        lm_head_weights = self.model_params["lm_head.weight"]
+        lm_head_weights = self.model_params[param_name]
 
         # Only care about weights connected to sequence tokens and none of the others
         seq_token_weights = torch.index_select(lm_head_weights, 0, self.sequence[:-1])
-        hidden_state_activations = self.model_activations["transformer"]["ln_f"].squeeze()
-
+    
         # Contribution of layer normed final hidden states to token logit
         contributions = torch.mul(seq_token_weights, hidden_state_activations)
-        self.contributions.append(("lm_head", contributions))
+        self.contributions.append((param_name, contributions))
         self.weight_product_sum = seq_token_weights
         
     def transformer_block_contributions(self, block_id):
@@ -107,7 +114,7 @@ class SequenceContributionCalculator:
     def mlp_contributions(self, block_id):
         # Last MLP layer input contribution to output, delta_x (activations), w_xy (weights)
         sub_block = "mlp"
-        layer_name = "dense_h_to_4h"
+        layer_name = "dense_4h_to_h"
         param_name = f"transformer.h.{block_id}.{sub_block}.{layer_name}.weight"
         mlp_dense_4h_to_h_contributions = self.feed_forward_contributions(
             block_id=block_id,
@@ -118,16 +125,16 @@ class SequenceContributionCalculator:
         self.contributions.append((param_name, mlp_dense_4h_to_h_contributions))
         
         # First MLP layer input contribution to output
-        sub_block = "self_attention"
-        layer_name = "dense"
+        sub_block = "mlp"
+        layer_name = "dense_h_to_4h"
         param_name = f"transformer.h.{block_id}.{sub_block}.{layer_name}.weight"
-        merged_head_to_4h_contributions = self.feed_forward_contributions( 
+        mlp_dense_h_to_4h_contributions = self.feed_forward_contributions( 
             block_id=block_id, 
             sub_block=sub_block,
             layer_name=layer_name, 
             param_name=param_name,
         )
-        self.contributions.append((param_name, merged_head_to_4h_contributions))
+        self.contributions.append((param_name, mlp_dense_h_to_4h_contributions))
         
     def feed_forward_contributions(self, block_id, sub_block, layer_name, param_name):
         # delta_x (activations), w_xy (weights)
@@ -164,6 +171,13 @@ class SequenceContributionCalculator:
         return contributions
     
     def multihead_atten_contributions(self, block_id):
+        # Merged multi-head attention contribution to dense layer outputs
+        sub_block = "self_attention"
+        layer_name = "dense"
+        param_name = f"transformer.h.{block_id}.{sub_block}.{layer_name}.weight"
+        merged_atten_head_contributions = self.feed_forward_contributions(block_id, sub_block, layer_name, param_name)
+        self.contributions.append((param_name, merged_atten_head_contributions))
+        
         value_contributions, value_weight_product_sum = self.value_layer_contributions(block_id)
         query_key_attn_weight_product_sum = self.query_key_atten_contributions(block_id)
         query_contributions, query_weight_product_sum = self.query_layer_contributions(block_id, query_key_attn_weight_product_sum)
@@ -317,6 +331,7 @@ class SequenceContributionCalculator:
 def main(model_size, data):
     attributor = NodeAttributor(model_size)
     contributions = attributor.calc_node_contributions(data)
+    #print(contributions)
     
     
 if __name__ == "__main__":